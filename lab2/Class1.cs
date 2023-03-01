b75dbb1aa570a15d156ac396dd859644c7a48511
167 <167@LAPTOP-P67DCS1R>
2023-03-01 14:49:32 +0300

    Добавьте файлы проекта.

---
+++ lab2/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        private char gender;
+        public char Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (value == 'M' || value == 'F')
+                {
+                    gender = value;
+                }
+                else
+                {
+                    throw new ArgumentException("пол должен быть  'M' or 'F'.");
+                }
+            }
+        }
+
+        public Person() { }
+
+        public Person(string lastName, string firstName, DateTime dateOfBirth, char gender)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            DateOfBirth = dateOfBirth;
+            Gender = gender;
+        }
+
+        public Person(Person person)
+        {
+            LastName = person.LastName;
+            FirstName = person.FirstName;
+            DateOfBirth = person.DateOfBirth;
+            Gender = person.Gender;
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите данные пользователя:");
+            Console.Write("фамилия: ");
+            LastName = Console.ReadLine();
+            Console.Write("имя: ");
+            FirstName = Console.ReadLine();
+            Console.Write("Дата рождения (ММ/дд/гггг): ");
+            DateOfBirth = DateTime.Parse(Console.ReadLine());
+            Console.Write("пол (M/F): ");
+            Gender = Console.ReadLine().ToUpper()[0];
+        }
+
+        public void Output()
+        {
+            Console.WriteLine($"имя: {FirstName} {LastName}");
+            Console.WriteLine($"день рождение: {DateOfBirth.ToShortDateString()}");
+            Console.WriteLine($"пол: {Gender}");
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName}, {DateOfBirth.ToShortDateString()}, {Gender}";
+        }
+
+        public int GetAge()
+        {
+            int age = DateTime.Today.Year - DateOfBirth.Year;
+            if (DateOfBirth.Date > DateTime.Today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
+
---
+++ lab2/Program.cs
@@ -0,0 +1,47 @@
+﻿using lab2;
+using System;
+class Program
+{
+    static void Task1()
+    {
+        Person person1 = new Person();
+        person1.Input();
+        Person person2 = new Person("Petrov", "Petr", new DateTime(1985, 5, 15), 'M');
+        Person person3 = new Person("Sidorova", "Maria", new DateTime(1999, 12, 31), 'F');
+        person1.Output();
+        person2.Output();
+        person3.Output();
+        string person1String = person1.ToString();
+        string person2String = person2.ToString();
+        string person3String = person3.ToString();
+        int age1 = person1.GetAge();
+        int age2 = person2.GetAge();
+        int age3 = person3.GetAge();
+
+    }
+    static void Task2()
+    {
+        Console.Write("Введите количество людей: ");
+        int count = int.Parse(Console.ReadLine());
+
+        Person[] people = new Person[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            people[i] = new Person();
+            people[i].Input();
+        }
+
+        Console.WriteLine("Данные о людях:");
+
+        foreach (Person person in people)
+        {
+            Console.WriteLine($"{person.LastName} {person.FirstName[0]}. {person.GetAge()} лет, {person.Gender}");
+        }
+    }
+    static void Main(string[] args)
+    {
+        Task2();
+        Task1();
+    }
+} 
